a51f5ef01a10349b101954a86d340b4ed9e585d3
potman99 <potmanv8@gmail.com>
2023-11-07 13:49:24 +0100

    change bg path

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,9 @@ export default {
         'headers': ['Rubik', 'sans-serif'],
       },
       backgroundImage: {
-        'wood-pattern': "url('./src/assets/wood_bg_mid.png')",
-        'map-pattern': "url('./src/assets/contact/map-bg.png')",
-        'about-vector': "url('./src/assets/about_us/vector.svg')"
+        'wood-pattern': "url('/src/assets/wood_bg_mid.png')",
+        'map-pattern': "url('/src/assets/contact/map-bg.png')",
+        'about-vector': "url('/src/assets/about_us/vector.svg')"
       },
       backgroundColor: {
         'lightBrown': 'rgba(255, 209, 125, 1)',
